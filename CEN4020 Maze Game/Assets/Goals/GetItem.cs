b44f2c015578ca121adc9568968ec1dbf790e694
Jason Hamilton <jasonvinh98@gmail.com>
2019-10-17 02:50:08 -0400

    Add goals interface
    Add interface for each objective to implement. Intended to simplify win conditions. Currently skeleton implementation and is not functional.

---
+++ CEN4020 Maze Game/Assets/Goals/GetItem.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Goals;
+
+public class GetItem : IGoal
+{
+    
+}
---
+++ CEN4020 Maze Game/Assets/Goals/IGoals.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Goals
+{
+
+    public interface IGoals : MonoBehaviour
+    {
+        bool IsComplete();
+        void Complete();
+        void Display();
+    }
+}
