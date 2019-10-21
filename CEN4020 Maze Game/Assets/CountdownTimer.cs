df703bcd41595dd74b4111d2d969e28ffb64b5e3
Abbey Centers <alc16j@my.fsu.edu>
2019-10-20 22:21:31 -0400

    Add documentation for Abbey's scripts
    This version does not contain the most updated features. However,
    it does contain well documented code for the TriggerEvent, CountdownTimer,
    and PlayerMovement scripts, which were all implemented by Abbey Centers.
    Documentation will be later merged with the updated version.

--- CEN4020 Maze Game/Assets/CountdownTimer.cs
+++ CEN4020 Maze Game/Assets/CountdownTimer.cs
@@ -6,37 +6,56 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+// this class is responsible for decrementing the countdown timer until it reaches 0 seconds
 
 public class CountdownTimer : MonoBehaviour
 {
     // game over text
     public GameObject gameOverText;
+
+    // winning text
     public GameObject winningText;
 
+    // time to display on the CountdownText object
     float currentTime;
+
+    // amount of time the player has to complete the level
     float startingTime = 20;
 
+    // private member data
     [SerializeField] Text countdownText;
 
+    // hide the game over text from the screen and
+    // set the initial start time of the timer
     private void Start()
     {
         gameOverText.SetActive(false);
         currentTime = startingTime;
     }
 
+    // update the countdownText once per frame
     private void Update()
     {
+        // decrease current time by 1 each second
         currentTime -= 1 * Time.deltaTime;
+
+        // convert to string without displaying decimal values
         countdownText.text = currentTime.ToString("0");
 
+        // do not display time less than 0 seconds
         if (currentTime <= 0)
         {
             Destroy(winningText);
             currentTime = 0;
+
+            // display to the user that the game is over
             gameOverText.SetActive(true);
             StartCoroutine("WaitForSec");
         }
     }
+
+    // destroy game over text and winning text so that neither can be 
+    // displayed again for this level
     IEnumerator WaitForSec()
     {
         yield return new WaitForSeconds(5);
--- CEN4020 Maze Game/Assets/PlayerMovement.cs
+++ CEN4020 Maze Game/Assets/PlayerMovement.cs
@@ -1,32 +1,36 @@
 ﻿// Abbey Centers
 // https://www.youtube.com/watch?v=whzomFgjT50
 
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// this class is responsible for handling player movement using the keyboard arrow keys
 public class PlayerMovement : MonoBehaviour
 {
+    // movement speed of the player
     public float moveSpeed = 5f;
 
+    // the player object that this script is attached to must have a Rigidbody 2D component
     public Rigidbody2D rb;
 
+    // used to handle x and y movements
     Vector2 movement;
-
 
     // Update is called once per frame
     void Update()
     {
-        // input
+        // get input from the user
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
     }
 
+    // update the position of the player
     void FixedUpdate() 
     { 
         // movement
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
-
     }
 
 }
