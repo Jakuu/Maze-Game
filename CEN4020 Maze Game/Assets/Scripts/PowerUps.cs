7496b1790dcc48da83fc466fa67a2e9b8b3509eb
Abbey Centers <alc16j@my.fsu.edu>
2019-11-03 09:33:51 -0500

    Add power-ups for animated tiles

---
+++ CEN4020 Maze Game/Assets/Scripts/PowerUps.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class PowerUps : MonoBehaviour
+{
+    //public GameObject pickupEffect;
+    public float multiplier = 1.4f;
+
+    public GameObject tilemapGameObject;
+    Tilemap tilemap;
+
+    void Start()
+    {
+        if (tilemapGameObject != null)
+        {
+            tilemap = tilemapGameObject.GetComponent<Tilemap>();
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D player)
+    {
+        Vector3 hitPosition = Vector3.zero;
+        if (tilemap != null && player.gameObject.tag == "Player")
+        {
+            hitPosition.x = player.transform.position.x;
+            hitPosition.y = player.transform.position.y;
+            tilemap.SetTile(tilemap.WorldToCell(hitPosition), null);
+
+            // apply effect to player
+            PlayerStats stats = player.GetComponent<PlayerStats>();
+            stats.score *= multiplier;
+        }
+    }
+}
+
+
+  
