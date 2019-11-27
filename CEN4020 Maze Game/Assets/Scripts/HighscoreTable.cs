89fec39e42d7aafd40139c97ba9f852431f7c969
Abbey Centers <alc16j@my.fsu.edu>
2019-11-27 13:53:38 -0500

    Add docs folder and status file
    Leaderboard for lvl1
    
    Current bug: When a new player entry is added to the table,
    some overlapping of text occurs every once in a while.
    
    Added new docs folder and status.txt file. status.txt is
    stored in the docs folder and contains the console output
    resulting from the 'git status' command.

--- CEN4020 Maze Game/Assets/Scripts/HighscoreTable.cs
+++ CEN4020 Maze Game/Assets/Scripts/HighscoreTable.cs
@@ -16,52 +16,25 @@ using UnityEngine;
 using UnityEngine.UI;
 using CodeMonkey.Utils;
 
-public class HighscoreTable : MonoBehaviour {
+public class HighscoreTable : MonoBehaviour
+{
 
     private Transform entryContainer;
     private Transform entryTemplate;
     public List<Transform> highscoreEntryTransformList;
 
-    public void Awake() {
-        entryContainer = transform.Find("highscoreEntryContainer");
-        entryTemplate = entryContainer.Find("highscoreEntryTemplate");
+    private void Awake()
+    {
 
-        entryTemplate.gameObject.SetActive(false);
+        //PlayerPrefs.DeleteAll();
+        updateTable();
 
-        string jsonString = PlayerPrefs.GetString("highscoreTable");
-        Highscores highscores = JsonUtility.FromJson<Highscores>(jsonString);
 
-        // clear entries
-        //ClearTable(highscoreEntryTransformList);
 
-        // Load high scores
-        //jsonString = PlayerPrefs.GetString("highscoreTable");
-        //highscores = JsonUtility.FromJson<Highscores>(jsonString);
-
-        // Sort entry list by Score
-        for (int i = 0; i < highscores.highscoreEntryList.Count; i++) {
-            for (int j = i + 1; j < highscores.highscoreEntryList.Count; j++) {
-                if (highscores.highscoreEntryList[j].score > highscores.highscoreEntryList[i].score) {
-                    // Swap
-                    HighscoreEntry tmp = highscores.highscoreEntryList[i];
-                    highscores.highscoreEntryList[i] = highscores.highscoreEntryList[j];
-                    highscores.highscoreEntryList[j] = tmp;
-                }
-            }
-        }
-
-        highscoreEntryTransformList = new List<Transform>();
-        int k = 0;
-        foreach (HighscoreEntry highscoreEntry in highscores.highscoreEntryList) {
-            if (k == 10)
-                break;
-            k++;
-            CreateHighscoreEntryTransform(highscoreEntry, entryContainer, highscoreEntryTransformList);
-        }
     }
 
 
-    private void ClearTable(List<Transform> list)
+    public void ClearTable(List<Transform> list)
     {
         for (int i = 0; i < list.Count; i++)
         {
@@ -70,8 +43,64 @@ public class HighscoreTable : MonoBehaviour {
     }
 
 
+    public void updateTable()
+    {
+        entryContainer = transform.Find("highscoreEntryContainer");
+        entryTemplate = entryContainer.Find("highscoreEntryTemplate");
 
-    private void CreateHighscoreEntryTransform(HighscoreEntry highscoreEntry, Transform container, List<Transform> transformList) {
+        entryTemplate.gameObject.SetActive(false);
+
+        string jsonString = PlayerPrefs.GetString("highscoreTable");
+        Highscores highscores = JsonUtility.FromJson<Highscores>(jsonString);
+
+
+        if (highscores == null)
+        {
+            // There's no stored table, initialize
+            highscores = new Highscores()
+            {
+                highscoreEntryList = new List<HighscoreEntry>()
+            };
+
+            // Reload
+            jsonString = PlayerPrefs.GetString("highscoreTable");
+            highscores = JsonUtility.FromJson<Highscores>(jsonString);
+        }
+
+
+        // Sort entry list by Score
+        if (highscores.highscoreEntryList.Count > 1)
+        {
+            for (int i = 0; i < highscores.highscoreEntryList.Count; i++)
+            {
+                for (int j = i + 1; j < highscores.highscoreEntryList.Count; j++)
+                {
+                    if (highscores.highscoreEntryList[j].score > highscores.highscoreEntryList[i].score)
+                    {
+                        // Swap
+                        HighscoreEntry tmp = highscores.highscoreEntryList[i];
+                        highscores.highscoreEntryList[i] = highscores.highscoreEntryList[j];
+                        highscores.highscoreEntryList[j] = tmp;
+                    }
+                }
+            }
+         }
+
+        highscoreEntryTransformList = new List<Transform>();
+        int k = 0;
+        foreach (HighscoreEntry highscoreEntry in highscores.highscoreEntryList)
+        {
+            if (k == 10)
+                break;
+            k++;
+            CreateHighscoreEntryTransform(highscoreEntry, entryContainer, highscoreEntryTransformList);
+        }
+    }
+
+
+
+    private void CreateHighscoreEntryTransform(HighscoreEntry highscoreEntry, Transform container, List<Transform> transformList)
+    {
         float templateHeight = 31f;
         Transform entryTransform = Instantiate(entryTemplate, container);
         RectTransform entryRectTransform = entryTransform.GetComponent<RectTransform>();
@@ -80,13 +109,14 @@ public class HighscoreTable : MonoBehaviour {
 
         int rank = transformList.Count + 1;
         string rankString;
-        switch (rank) {
-        default:
-            rankString = rank + "TH"; break;
+        switch (rank)
+        {
+            default:
+                rankString = rank + "TH"; break;
 
-        case 1: rankString = "1ST"; break;
-        case 2: rankString = "2ND"; break;
-        case 3: rankString = "3RD"; break;
+            case 1: rankString = "1ST"; break;
+            case 2: rankString = "2ND"; break;
+            case 3: rankString = "3RD"; break;
         }
 
         entryTransform.Find("posText").GetComponent<Text>().text = rankString;
@@ -100,45 +130,50 @@ public class HighscoreTable : MonoBehaviour {
 
         // Set background visible odds and evens, easier to read
         entryTransform.Find("background").gameObject.SetActive(rank % 2 == 1);
-        
+
         // Highlight First
-        if (rank == 1) {
+        if (rank == 1)
+        {
             entryTransform.Find("posText").GetComponent<Text>().color = Color.green;
             entryTransform.Find("scoreText").GetComponent<Text>().color = Color.green;
             entryTransform.Find("nameText").GetComponent<Text>().color = Color.green;
         }
 
         // Set tropy
-        switch (rank) {
-        default:
-            entryTransform.Find("trophy").gameObject.SetActive(false);
-            break;
-        case 1:
-            entryTransform.Find("trophy").GetComponent<Image>().color = UtilsClass.GetColorFromString("FFD200");
-            break;
-        case 2:
-            entryTransform.Find("trophy").GetComponent<Image>().color = UtilsClass.GetColorFromString("C6C6C6");
-            break;
-        case 3:
-            entryTransform.Find("trophy").GetComponent<Image>().color = UtilsClass.GetColorFromString("B76F56");
-            break;
+        switch (rank)
+        {
+            default:
+                entryTransform.Find("trophy").gameObject.SetActive(false);
+                break;
+            case 1:
+                entryTransform.Find("trophy").GetComponent<Image>().color = UtilsClass.GetColorFromString("FFD200");
+                break;
+            case 2:
+                entryTransform.Find("trophy").GetComponent<Image>().color = UtilsClass.GetColorFromString("C6C6C6");
+                break;
+            case 3:
+                entryTransform.Find("trophy").GetComponent<Image>().color = UtilsClass.GetColorFromString("B76F56");
+                break;
 
         }
 
         transformList.Add(entryTransform);
     }
 
-    public void AddHighscoreEntry(int score, string name) {
+    public void AddHighscoreEntry(int score, string name)
+    {
         // Create HighscoreEntry
         HighscoreEntry highscoreEntry = new HighscoreEntry { score = score, name = name };
-        
+
         // Load saved Highscores
         string jsonString = PlayerPrefs.GetString("highscoreTable");
         Highscores highscores = JsonUtility.FromJson<Highscores>(jsonString);
 
-        if (highscores == null) {
+        if (highscores == null)
+        {
             // There's no stored table, initialize
-            highscores = new Highscores() {
+            highscores = new Highscores()
+            {
                 highscoreEntryList = new List<HighscoreEntry>()
             };
         }
@@ -152,15 +187,17 @@ public class HighscoreTable : MonoBehaviour {
         PlayerPrefs.Save();
     }
 
-    private class Highscores {
+    private class Highscores
+    {
         public List<HighscoreEntry> highscoreEntryList;
     }
 
     /*
      * Represents a single High score entry
      * */
-    [System.Serializable] 
-    private class HighscoreEntry {
+    [System.Serializable]
+    private class HighscoreEntry
+    {
         public int score;
         public string name;
     }
--- CEN4020 Maze Game/Assets/Scripts/TriggerEvent.cs
+++ CEN4020 Maze Game/Assets/Scripts/TriggerEvent.cs
@@ -4,6 +4,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 // this class is responsible for displaying a victory message to the user
 // if the maze exit is found and the "item retrieval" objective
@@ -15,8 +16,14 @@ public class TriggerEvent : MonoBehaviour
     public GameObject player;
     public GameObject highScoreTable;
 
+    // get name from user
+    [SerializeField] Text leaderboardName;
+
     // high score UI
     [SerializeField] GameObject HighScoreUI;
+
+    // leaderboard name UI
+    [SerializeField] GameObject GetLeaderboardNameUI;
 
     // winning text to display
     [SerializeField] GameObject winMenu;
@@ -35,6 +42,7 @@ public class TriggerEvent : MonoBehaviour
     private void Start()
     {
         winMenu.SetActive(false);
+        GetLeaderboardNameUI.SetActive(false);
         HighScoreUI.SetActive(false);
         deadMenu.SetActive(false);
     }
@@ -47,12 +55,12 @@ public class TriggerEvent : MonoBehaviour
         // if the player is equal to our Player object
         if (player.gameObject.tag == "Player")
         {
+
             // if the key has been collected / the objective has been completed
             //if (keyObj.isComplete())
             //{
                 // destroy text that could display game over message
                 // display victory message to the user
-
 
             // get player score
             PlayerStats stats = player.GetComponent<PlayerStats>();
@@ -61,72 +69,114 @@ public class TriggerEvent : MonoBehaviour
             HighscoreTable leaderboard = highScoreTable.GetComponent<HighscoreTable>();
 
             // get min of top ten leaderboard scores
-
             string jsonString = PlayerPrefs.GetString("highscoreTable");
             Highscores highscores = JsonUtility.FromJson<Highscores>(jsonString);
+
+
             // Sort entry list by Score
-            for (int i = 0; i < highscores.highscoreEntryList.Count; i++)
+            if (highscores != null)
             {
-                for (int j = i + 1; j < highscores.highscoreEntryList.Count; j++)
+                if (highscores.highscoreEntryList.Count > 1)
                 {
-                    if (highscores.highscoreEntryList[j].score > highscores.highscoreEntryList[i].score)
+                    for (int i = 0; i < highscores.highscoreEntryList.Count; i++)
                     {
-                        // Swap
-                        HighscoreEntry tmp = highscores.highscoreEntryList[i];
-                        highscores.highscoreEntryList[i] = highscores.highscoreEntryList[j];
-                        highscores.highscoreEntryList[j] = tmp;
+                        for (int j = i + 1; j < highscores.highscoreEntryList.Count; j++)
+                        {
+                            if (highscores.highscoreEntryList[j].score > highscores.highscoreEntryList[i].score)
+                            {
+                                // Swap
+                                HighscoreEntry tmp = highscores.highscoreEntryList[i];
+                                highscores.highscoreEntryList[i] = highscores.highscoreEntryList[j];
+                                highscores.highscoreEntryList[j] = tmp;
+                            }
+                        }
                     }
                 }
+
+
+
+
+                // get min of top ten scores
+                int min;
+                if (highscores.highscoreEntryList.Count < 10)
+                {
+                    Debug.Log("less than 10");
+                    min = highscores.highscoreEntryList[highscores.highscoreEntryList.Count - 1].score;
+                }
+                else
+                {
+                    Debug.Log("greater than 10");
+                    min = highscores.highscoreEntryList[9].score;
+                }
+
+
+
+
+                Debug.Log("min: " + min);
             }
-
-            // get tenth index
-            int min = highscores.highscoreEntryList[9].score;
-
-            Debug.Log("min: " + min);
-
-           
-           
-            if (stats.score > min)
-            {
-                Debug.Log("player in top ten");
-                // add user to leaderboard ONLY IF THEY ARE IN TOP 10
-                leaderboard.AddHighscoreEntry(stats.score, "Abbey");
-                leaderboard.Awake();
-            
-            
-            }
-
-            
-
-            // get min and max of leaderboard
-
-            // check if score is in top ten
-
-            // if yes, ask for user input
-
-            // create canvas similar to highscore table
-
-            // set it to active
-
-            // on key enter pressed, get input and add to highscore entry
-
-
-
-
-
-
 
             winMenu.SetActive(true);
-            HighScoreUI.SetActive(true);
             pauseMenu.SetActive(false);
             canvasPause.SetActive(true);
             Time.timeScale = 0f;
             PauseMenu.IsPaused = true;
+            HighScoreUI.SetActive(false);
 
+           
+            //if (stats.score > min)
+            {
+                // get player name for high score table
+                StartCoroutine(getName(stats, leaderboard));            
+            }
 
-            //}
         }
     }
+
+
+    private IEnumerator getName(PlayerStats stats, HighscoreTable leaderboard)
+    {
+        // show enter name prompt
+        GetLeaderboardNameUI.SetActive(true);
+
+
+        // just a simple time delay as an example
+        //yield return new WaitForSeconds(2.5f);
+
+        // wait for player to press space
+        yield return waitForKeyPress(KeyCode.Return, stats, leaderboard); // wait for this function to return
+
+        // remove prompt
+        GetLeaderboardNameUI.SetActive(false);
+    }
+
+    private IEnumerator waitForKeyPress(KeyCode key, PlayerStats stats, HighscoreTable leaderboard)
+    {
+        bool done = false;
+        while (!done) // essentially a "while true", but with a bool to break out naturally
+        {
+            if (Input.GetKeyDown(key))
+            {
+                done = true; // breaks the loop
+
+                // add user to leaderboard ONLY IF THEY ARE IN TOP 10
+
+                // if only one entry and the score for that entry is, remove it
+                // this is only used to initialize the table
+               // if (leaderboard.highscoreEntryList.Count == 1 && leaderboard.highscoreEntyrList[0].score == -1)
+                 //   leaderboard.ClearTable();
+                
+                leaderboard.AddHighscoreEntry(stats.score, leaderboardName.text);
+                leaderboard.updateTable();
+                HighScoreUI.SetActive(true);
+
+
+            }
+            yield return null; // wait until next frame, then continue execution from here (loop continues)
+        }
+
+        // now this function returns
+    }
+
 
 
 
