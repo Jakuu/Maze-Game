cc18fbd4903ad9c1e4f718bd003a39cb233a8285
Abbey Centers <alc16j@my.fsu.edu>
2019-11-29 14:33:19 -0500

    Ridiculous fourth level with gravity

---
+++ CEN4020 Maze Game/Assets/Scripts/Clock_Weighted.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+// Abbey Centers
+// source: https://www.youtube.com/watch?v=CLSiRf_OrBk
+
+public class Clock_Weighted : MonoBehaviour
+{
+    // timer text to update
+    public CountdownTimer timer;
+
+    // action performed when player collides with one of the clocks
+    void OnCollisionEnter2D(Collision2D player)
+    {
+        if (player.gameObject.tag == "Player")
+        {
+            // apply effect to timer
+            timer.AddTime(5.0f);
+
+            // destroy clock object
+            Destroy(gameObject);
+        }
+    }
+}
+
+
+
---
+++ CEN4020 Maze Game/Assets/Scripts/PowerUp_Weighted.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUp_Weighted : MonoBehaviour
+{
+    public int add = 25;
+    public GameObject player;
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Player")
+            PickUp();
+    }
+
+    void PickUp()
+    {
+        // apply effect to player
+        PlayerStats stats = player.GetComponent<PlayerStats>();
+        if (stats.score == 0)
+            stats.score = 25;
+
+        stats.score += add;
+        Destroy(gameObject);
+    }
+}
